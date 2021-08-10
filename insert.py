0cb56e8b199e8844e478915f6b4857b64025de5c
bohamian <s4616@naver.com>
2021-08-10 20:54:17 +0900

    장애프로그램

--- insert.py
+++ insert.py
@@ -6,7 +6,7 @@ data1, data2, data3, data4 = "", "", "", ""
 sql=""
 
 # 메인 코드
-conn = pymysql.connect(host='127.0.0.1', user='root', password='1234', db='memberDB', charset='utf8')
+conn = pymysql.connect( )
 cur = conn.cursor()
 
 while (True) :
@@ -16,7 +16,7 @@ while (True) :
     data2 = input("사용자 이름 ==> ")
     data3 = input("사용자 이메일 ==> ")
     data4 = input("사용자 출생연도 ==> ")
-    sql = "INSERT INTO userTable VALUES('" + data1 + "','" + data2 + "','" + data3 + "'," + data4 + ")"
+    sql = " "
     cur.execute(sql)
 
 conn.commit()
